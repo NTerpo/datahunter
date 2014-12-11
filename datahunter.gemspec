20e1d6613103bb16486a861888de56f3a3c984c2
Terpo <nicolas.terpolilli@gmail.com>
2014-12-11 15:12:00 +0100

    first little test to ensure travis works

--- datahunter.gemspec
+++ datahunter.gemspec
@@ -16,6 +16,7 @@ Gem::Specification.new do |spec|
   spec.files         = `git ls-files -z`.split("\x0")
   spec.files.reject! { |fn| fn.include? "gif" }
   spec.files.reject! { |fn| fn.include? "logo" }
+  spec.files.reject! { |fn| fn.include? "travis" }
   spec.executables   = spec.files.grep(%r{^bin/}) { |f| File.basename(f) }
   spec.test_files    = spec.files.grep(%r{^(test|spec|features)/})
   spec.require_paths = ["lib"]
--- spec/datahunter_spec.rb
+++ spec/datahunter_spec.rb
@@ -1,10 +1,15 @@
 require 'spec_helper'
 
-describe Datahunter do 
+describe "Datahunter Base" do 
 
-  describe 'datasets_url' do
-    it 'returns a correct url' do
-      pending
+  before(:each) do
+    DATASETS_URL = "http://shrouded-harbor-5877.herokuapp.com/api/datasets/"
+  end
+  
+  describe 'create correct datasets_url' do
+    it 'should return a correct url' do
+      url = Datahunter.datasets_url("population", "france", "2010")
+      expect(url).to eq("#{DATASETS_URL}?tags=population&spatial=france&temporal=2010")
     end
   end
 end
