9eab2c6cd35392cfed32233db7206c353b2710ce
Terpo <nicolas.terpolilli@gmail.com>
2014-12-15 20:41:11 +0100

    started tests for hunter CLI

---
+++ spec/hunter_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+describe 'Hunter CLI' do
+
+    
+  it 'find command should be working' do
+  
+  end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,1 +1,14 @@
 require 'datahunter'
+require 'aruba/rspec'
+
+RSpec.configure do |config|
+  config.include ArubaDoubles
+
+  config.before :each do
+    Aruba::RSpec.setup
+  end
+
+  config.after :each do
+    Aruba::RSpec.teardown
+  end
+end
