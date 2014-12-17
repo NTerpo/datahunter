722a19c4ce3d64fbd83ae6cff25e4a82379f01ee
Terpo <nicolas.terpolilli@gmail.com>
2014-12-17 12:15:48 +0100

    fix HTTP Code when no content + remove API calls specs

--- spec/datahunter_spec.rb
+++ spec/datahunter_spec.rb
@@ -13,33 +13,6 @@ describe "Datahunter API calls" do
     end
   end
 
-  describe 'Should get the right HTTP codes when calling the API' do
-    response_ok = RestClient.get("#{DATASETS_URL}?tags=population",
-                                 :content_type => :json)
-    response_ni = RestClient.get(DATASETS_URL,
-                                 :content_type => :json){|response, request, result| response}
-    response_nf = RestClient.get("#{DATASETS_URL}?tags=foo",
-                                 :content_type => :json){|response, request, result| response}
-    response_nff = RestClient.get("http://shrouded-harbor-5877.herokuapp.com/api/foo",
-                                 :content_type => :json){|response, request, result| response}
-    
-     it 'should return an HTTP 200 with a correct url' do
-      response_ok.code.should eq(200)
-    end
-
-    it 'should return an HTTP 501 when calling api/datasets/' do
-      response_ni.code.should eq(501)
-    end
-
-    it 'should return an HTTP 500 when a query has no result' do
-      response_nf.code.should eq(500)
-    end
-
-    it 'should return an HTTP 404 when calling a non defined URL' do
-      response_nff.code.should eq(404)
-    end
-  end
-
   describe 'Should receive a dataset corresponding to the query' do
     response = RestClient.get("#{DATASETS_URL}?tags=population",
                               :content_type => :json)
